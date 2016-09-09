7f5c07572edaf102419543c46e27eac361ed7cd2
Angel Nieves <13354095+Sartron@users.noreply.github.com>
2016-09-08 23:43:37 -0400

    Tournament GET requests - Supports requesting participants and matches
    Participant GET requests
    - GetParticipant() works, incuding the parameters for requesting matches
    
    Class 'ChallongeMatch' created, needs to be implemented into the GET request functions
    Added Helpers.cs which includes functions for verifying LINQ and JSON objects

--- bracketAPI/Challonge/ChallongeClasses.cs
+++ bracketAPI/Challonge/ChallongeClasses.cs
@@ -1,14 +1,8 @@
-﻿using RestSharp.Deserializers;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Xml.XPath;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace bracketAPI.Challonge
 {
@@ -42,21 +36,29 @@ namespace bracketAPI.Challonge
             rawJSON = jObject;
 
             //DateTime Properties
-            if (!string.IsNullOrWhiteSpace((string)jObject["tournament"]["completed_at"])) Completed = DateTime.Parse((string)jObject["tournament"]["completed_at"]);
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["completed_at"])) Completed = DateTime.Parse((string)jObject["tournament"]["completed_at"]);
             Created = DateTime.Parse((string)jObject["tournament"]["created_at"]);
-            if (!string.IsNullOrWhiteSpace((string)jObject["tournament"]["started_at"])) Started = DateTime.Parse((string)jObject["tournament"]["started_at"]);
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["started_at"])) Started = DateTime.Parse((string)jObject["tournament"]["started_at"]);
             Updated = DateTime.Parse((string)jObject["tournament"]["updated_at"]);
 
             //Class Properties
-            if (jObject["tournament"]["matches"] != null)
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["matches"]))
             {
-                //Parse matches
-                Debug.WriteLine("Match array found");
+                //Create list
+                Matches = new List<ChallongeMatch>();
+
+                //Add participants to list
+                foreach (JObject match in jObject["tournament"]["matches"])
+                    Matches.Add(new ChallongeMatch(match));
             }
-            if (jObject["tournament"]["participants"] != null)
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["participants"]))
             {
-                //Parse participants
-                Debug.WriteLine("Tournament array found");
+                //Create list
+                Participants = new List<ChallongeParticipant>();
+
+                //Add participants to list
+                foreach (JObject participant in jObject["tournament"]["participants"])
+                    Participants.Add(new ChallongeParticipant(participant));
             }
 
             //Enum Properties
@@ -68,16 +70,16 @@ namespace bracketAPI.Challonge
             ImageURL = (Uri)jObject["tournament"]["live_image_url"];
 
             //int Properties
-            if (!string.IsNullOrWhiteSpace((string)jObject["tournament"]["game_id"])) GameID = (int)jObject["tournament"]["game_id"];
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["game_id"])) GameID = (int)jObject["tournament"]["game_id"];
             ID = (int)jObject["tournament"]["id"];
             Progress = (int)jObject["tournament"]["progress_meter"];
 
             //string Properties
             Description = (string)jObject["tournament"]["description"];
-            if (!string.IsNullOrWhiteSpace((string)jObject["tournament"]["game_name"])) GameName = (string)jObject["tournament"]["game_name"];
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["game_name"])) GameName = (string)jObject["tournament"]["game_name"];
             Name = (string)jObject["tournament"]["name"];
             ShortURL = (string)jObject["tournament"]["url"];
-            if (!string.IsNullOrWhiteSpace((string)jObject["tournament"]["subdomain"])) Subdomain = (string)jObject["tournament"]["subdomain"];
+            if (!Helpers.IsNullOrEmpty(jObject["tournament"]["subdomain"])) Subdomain = (string)jObject["tournament"]["subdomain"];
         }
 
         public ChallongeTournament(XElement xElement)
@@ -86,13 +88,30 @@ namespace bracketAPI.Challonge
             rawXML = xElement;
 
             //DateTime Properties
-            if (!string.IsNullOrWhiteSpace(xElement.Element("completed-at").Value)) Completed = DateTime.Parse(xElement.Element("completed-at").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("completed-at"))) Completed = DateTime.Parse(xElement.Element("completed-at").Value);
             Created = DateTime.Parse(xElement.Element("created-at").Value);
-            if (!string.IsNullOrWhiteSpace(xElement.Element("started-at").Value)) Completed = DateTime.Parse(xElement.Element("started-at").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("started-at"))) Completed = DateTime.Parse(xElement.Element("started-at").Value);
             Updated = DateTime.Parse(xElement.Element("updated-at").Value);
 
             //Class Properties
-            //
+            if (!Helpers.IsNullOrEmpty(xElement.Element("matches")))
+            {
+                //Create list
+                Matches = new List<ChallongeMatch>();
+
+                //Add participants to list
+                foreach (XElement match in xElement.Element("matches").Elements())
+                    Matches.Add(new ChallongeMatch(match));
+            }
+            if (!Helpers.IsNullOrEmpty(xElement.Element("participants")))
+            {
+                //Create list
+                Participants = new List<ChallongeParticipant>();
+
+                //Add participants to list
+                foreach (XElement participant in xElement.Element("participants").Elements())
+                    Participants.Add(new ChallongeParticipant(participant));
+            }
 
             //Enum Properties
             State = ChallongeHelpers.ParseTournamentState(xElement.Element("state").Value);
@@ -103,42 +122,140 @@ namespace bracketAPI.Challonge
             ImageURL = new Uri(xElement.Element("live-image-url").Value);
 
             //int Properties
-            if (!string.IsNullOrWhiteSpace(xElement.Element("game-id").Value)) GameID = Convert.ToInt32(xElement.Element("game-id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("game-id"))) GameID = Convert.ToInt32(xElement.Element("game-id").Value);
             ID = Convert.ToInt32(xElement.Element("id").Value);
             Progress = Convert.ToInt32(xElement.Element("progress-meter").Value);
 
             //string Properties
             Description = xElement.Element("description").Value;
-            if (!string.IsNullOrWhiteSpace(xElement.Element("game-name").Value)) GameName = xElement.Element("game-name").Value;
+            if (!Helpers.IsNullOrEmpty(xElement.Element("game-name"))) GameName = xElement.Element("game-name").Value;
             Name = xElement.Element("name").Value;
             ShortURL = xElement.Element("url").Value;
-            if (!string.IsNullOrWhiteSpace(xElement.Element("subdomain").Value)) Subdomain = xElement.Element("subdomain").Value;
+            if (!Helpers.IsNullOrEmpty(xElement.Element("subdomain"))) Subdomain = xElement.Element("subdomain").Value;
         }
     }
 
     public class ChallongeParticipant
     {
+        private JObject rawJSON;
+        private XElement rawXML;
+
+        public List<ChallongeMatch> Matches { get; }
+        public int ParticipantID { get; }
+        public int Placing { get; }
+        public int Seed { get; }
+        public int TournamentID { get; }
+        public string ChallongeUsername { get; }
+        public string PlayerName { get; }
+
         public ChallongeParticipant(JObject jObject)
         {
+            //Store raw JSON
+            rawJSON = jObject;
 
+            //Class Properties
+            if (!Helpers.IsNullOrEmpty(jObject["participant"]["matches"]))
+            {
+                //Create list
+                Matches = new List<ChallongeMatch>();
+
+                //Add participants to list
+                foreach (JObject match in jObject["participant"]["matches"])
+                    Matches.Add(new ChallongeMatch(match));
+            }
+
+            //int Properties
+            ParticipantID = (int)jObject["participant"]["id"];
+            if (!Helpers.IsNullOrEmpty(jObject["participant"]["final_rank"])) Placing = (int)jObject["participant"]["final_rank"];
+            Seed = (int)jObject["participant"]["seed"];
+            TournamentID = (int)jObject["participant"]["tournament_id"];
+
+            //string Properties
+            if (!Helpers.IsNullOrEmpty(jObject["participant"]["challonge_username"])) ChallongeUsername = (string)jObject["participant"]["challonge_username"];
+            PlayerName = (string)jObject["participant"]["name"];
         }
 
         public ChallongeParticipant(XElement xElement)
         {
+            //Store raw XML
+            rawXML = xElement;
 
+            //Class Properties
+            if (!Helpers.IsNullOrEmpty(xElement.Element("matches")))
+            {
+                //Create list
+                Matches = new List<ChallongeMatch>();
+
+                //Add participants to list
+                foreach (XElement match in xElement.Element("matches").Elements())
+                    Matches.Add(new ChallongeMatch(match));
+            }
+
+            //int Properties
+            ParticipantID = Convert.ToInt32(xElement.Element("id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("final-rank"))) Placing = Convert.ToInt32(xElement.Element("final-rank").Value);
+            Seed = Convert.ToInt32(xElement.Element("seed").Value);
+            TournamentID = Convert.ToInt32(xElement.Element("tournament-id").Value);
+
+            //string Properties
+            if (!Helpers.IsNullOrEmpty(xElement.Element("challonge_username"))) ChallongeUsername = xElement.Element("challonge_username").Value;
+            PlayerName = xElement.Element("name").Value;
         }
     }
 
     public class ChallongeMatch
     {
+        private JObject rawJSON;
+        private XElement rawXML;
+
+        public int LoserParticipantID { get; }
+        public int MatchID { get; }
+        public int Participant1_ID { get; }
+        public int Participant1_PreReq_MatchID { get; }
+        public int Participant2_ID { get; }
+        public int Participant2_PreReq_MatchID { get; }
+        public int Round { get; }
+        public int TournamentID { get; }
+        public int WinnerParticipantID { get; }
+
         public ChallongeMatch(JObject jObject)
         {
+            //Store raw JSON
+            rawJSON = jObject;
 
+            //Class Properties
+            //
+
+            //int Properties
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["loser_id"])) LoserParticipantID = (int)jObject["match"]["loser_id"];
+            MatchID = (int)jObject["match"]["id"];
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["player1_id"])) Participant1_ID = (int)jObject["match"]["player1_id"];
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["player1_prereq_match_id"])) Participant1_PreReq_MatchID = (int)jObject["match"]["player1_prereq_match_id"];
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["player2_id"])) Participant2_ID = (int)jObject["match"]["player2_id"];
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["player2_prereq_match_id"])) Participant2_PreReq_MatchID = (int)jObject["match"]["player2_prereq_match_id"];
+            Round = (int)jObject["match"]["round"];
+            TournamentID = (int)jObject["match"]["tournament_id"];
+            if (!Helpers.IsNullOrEmpty(jObject["match"]["winner_id"])) WinnerParticipantID = (int)jObject["match"]["winner_id"];
         }
 
         public ChallongeMatch(XElement xElement)
         {
+            //Store raw XML
+            rawXML = xElement;
 
+            //Class Properties
+            //
+
+            //int Properties
+            if (!Helpers.IsNullOrEmpty(xElement.Element("loser-id"))) LoserParticipantID = Convert.ToInt32(xElement.Element("loser-id").Value);
+            MatchID = Convert.ToInt32(xElement.Element("id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("player1-id"))) Participant1_ID = Convert.ToInt32(xElement.Element("player1-id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("player1-prereq-match-id"))) Participant1_PreReq_MatchID = Convert.ToInt32(xElement.Element("player1-prereq-match-id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("player2-id"))) Participant2_ID = Convert.ToInt32(xElement.Element("player2-id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("player2-prereq-match-id"))) Participant2_PreReq_MatchID = Convert.ToInt32(xElement.Element("player2-prereq-match-id").Value);
+            Round = Convert.ToInt32(xElement.Element("round").Value);
+            TournamentID = Convert.ToInt32(xElement.Element("tournament-id").Value);
+            if (!Helpers.IsNullOrEmpty(xElement.Element("winner_id"))) WinnerParticipantID = Convert.ToInt32(xElement.Element("winner_id").Value);
         }
     }
 }
---
+++ bracketAPI/Helpers.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Xml.Linq;
+
+namespace bracketAPI
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Returns a boolean indicating whether or not the JToken is null or empty
+        /// </summary>
+        public static bool IsNullOrEmpty(JToken jToken)
+        {
+            return (jToken == null) ||
+                   (jToken.Type == JTokenType.Array && !jToken.HasValues) ||
+                   (jToken.Type == JTokenType.Object && !jToken.HasValues) ||
+                   (jToken.Type == JTokenType.String && jToken.ToString() == String.Empty) ||
+                   (jToken.Type == JTokenType.Null);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether or not the JObject is null or empty
+        /// </summary>
+        public static bool IsNullOrEmpty(JObject jObject)
+        {
+            JToken jToken = jObject;
+            return (jToken == null) ||
+                   (jToken.Type == JTokenType.Array && !jToken.HasValues) ||
+                   (jToken.Type == JTokenType.Object && !jToken.HasValues) ||
+                   (jToken.Type == JTokenType.String && jToken.ToString() == String.Empty) ||
+                   (jToken.Type == JTokenType.Null);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether or not the XElement is null or empty
+        /// </summary>
+        public static bool IsNullOrEmpty(XElement xElement)
+        {
+            return (xElement == null) ||
+                (xElement.IsEmpty) ||
+                (string.IsNullOrEmpty(xElement.Value));
+        }
+    }
+}
